4cfb3a67237203d804c16d61490eeb3bd10dca67
Melo La Mantia <carmelo.la.mantia00@gmail.com>
2023-04-25 00:54:28 +0200

    add logo + favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import logo from '../images/sushi.png'
 
 //NavBar Component
-function Navbar(props){
+function Navbar(){
     return (
         
         <nav className="navbar navbar-dark bg-dark">
             <div className="container-fluid">
-                <span className="navbar-brand mb-0 h1">Sushi Project</span>
+                <span className="navbar-brand mb-0 h1">
+                <img src={logo} className="size_sm me-2" alt="sushi" />
+                    Sushi Project
+
+                </span>
 
                 <div>
                 </div>
